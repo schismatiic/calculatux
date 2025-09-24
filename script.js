fa6401e173e6d2c0ed1b068bcf6cc4fd6a8e69c4
schismatiic <leonardojofre2003@gmail.com>
2025-09-24 08:58:14 -0300

    Función operate creada
    La función operate toma dos valores y un operador y llama a la función que corresponda con la operación

--- script.js
+++ script.js
@@ -2,3 +2,22 @@ const add = (a, b) => a + b;
 const subtract = (a, b) => a - b;
 const multiply = (a, b) => a * b;
 const divide = (a, b) => a / b;
+const operate = (a, b, operator) => {
+  switch (operator) {
+    case "+":
+      return add(a, b);
+      break;
+    case "-":
+      return subtract(a, b);
+      break;
+    case "*":
+      return multiply(a, b);
+      break;
+    case "/":
+      return divide(a, b);
+      break;
+    default:
+      console.log("ERROR!");
+      break;
+  }
+};
