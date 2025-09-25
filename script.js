7faa778185d3644ddb5282da6ab116acbba4ec7f
schismatiic <leonardojofre2003@gmail.com>
2025-09-25 14:59:43 -0300

    Botón AC y root square funcionales
    Ambos botones funcionan. Ahora queda hacer que la calculadora acepte números que no sean unidades

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ const add = (a, b) => a + b;
 const subtract = (a, b) => a - b;
 const multiply = (a, b) => a * b;
 const divide = (a, b) => a / b;
-const rootSquare = (a) => Math.sqrt(a);
+const squareRoot = (a) => Math.sqrt(a);
 const pow = (a, b) => a ** b;
 const operate = (a, b, operator) => {
   switch (operator) {
@@ -17,9 +17,6 @@ const operate = (a, b, operator) => {
       break;
     case "/":
       return divide(a, b);
-      break;
-    case "√x":
-      return rootSquare(a);
       break;
     case "^":
       return pow(a, b);
@@ -36,48 +33,47 @@ const ac = () => {
 // const num2 = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
 const operators = ["+", "-", "*", "/", "√x", "^"];
 const display = document.getElementById("display");
-const acButton = document.getElementById("ac__button");
 const operation = document.getElementById("operation");
+// ========= AC Button =========
+const acButton = document.getElementById("ac__button");
+acButton.addEventListener("click", () => {
+  operation.style.cssText = "color: #8f8f8f; font-weight: 400";
+  display.textContent = 0;
+  calculator();
+});
+// =============================
 const calculator = () => {
   for (let index = 0; index <= 9; index++) {
-    acButton.addEventListener("click", () => {
-      display.textContent = 0;
-      calculator();
-    });
     let numberButtons = document.getElementById(`${index}`);
     const equalButton = document.getElementById("equal__button");
     numberButtons.addEventListener("click", () => {
       display.textContent = `${index}`;
       const num1 = parseInt(`${index}`);
       for (let i = 0; i < 6; i++) {
-        acButton.addEventListener("click", () => {
-          display.textContent = 0;
-          calculator();
-        });
         const operatorButtons = document.getElementById(`${operators[i]}`);
         operatorButtons.addEventListener("click", () => {
           const operator = `${operators[i]}`;
-          display.textContent = `${num1} ${operator}`;
-          for (let j = 0; j <= 9; j++) {
-            acButton.addEventListener("click", () => {
-              display.textContent = 0;
-              calculator();
+          if (`${operator}` === "√x") {
+            display.textContent = `${num1} ${operator}`;
+            equalButton.addEventListener("click", () => {
+              operation.textContent = `${num1} ${operator} =`;
+              operation.style.cssText = "color: black; font-weight: 400";
+              display.textContent = `${squareRoot(num1)}`;
             });
-            let numberButtons2 = document.getElementById(`${j}`);
-            numberButtons2.addEventListener("click", () => {
-              acButton.addEventListener("click", () => {
-                display.textContent = 0;
-                operation.style.cssText = "color: #8f8f8f; font-weight: 400";
-                calculator();
+          } else {
+            display.textContent = `${num1} ${operator}`;
+            for (let j = 0; j <= 9; j++) {
+              let numberButtons2 = document.getElementById(`${j}`);
+              numberButtons2.addEventListener("click", () => {
+                const num2 = parseInt(`${j}`);
+                display.textContent = `${num1} ${operator} ${num2}`;
+                equalButton.addEventListener("click", () => {
+                  operation.textContent = `${num1} ${operator} ${num2} =`;
+                  operation.style.cssText = "color: black; font-weight: 400";
+                  display.textContent = `${operate(num1, num2, operator)}`;
+                });
               });
-              const num2 = parseInt(`${j}`);
-              display.textContent = `${num1} ${operator} ${num2}`;
-              equalButton.addEventListener("click", () => {
-                operation.textContent = `${num1} ${operator} ${num2} =`;
-                operation.style.cssText = "color: black; font-weight: 400";
-                display.textContent = `${operate(num1, num2, operator)}`;
-              });
-            });
+            }
           }
         });
       }
