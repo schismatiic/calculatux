8578d26b878edb92112501dff1531b8ba36157e4
schismatiic <leonardojofre2003@gmail.com>
2025-09-25 13:43:21 -0300

    Potenciación implementada. AC y raíz cuadrada en proceso
    Potenciación funciona perfectamente, pero AC hace que el programa vaya más lento y squareRoot tiene errores visuales

--- script.js
+++ script.js
@@ -2,6 +2,8 @@ const add = (a, b) => a + b;
 const subtract = (a, b) => a - b;
 const multiply = (a, b) => a * b;
 const divide = (a, b) => a / b;
+const rootSquare = (a) => Math.sqrt(a);
+const pow = (a, b) => a ** b;
 const operate = (a, b, operator) => {
   switch (operator) {
     case "+":
@@ -16,56 +18,70 @@ const operate = (a, b, operator) => {
     case "/":
       return divide(a, b);
       break;
+    case "√x":
+      return rootSquare(a);
+      break;
+    case "^":
+      return pow(a, b);
+      break;
     default:
       console.log("ERROR!");
       break;
   }
 };
+const ac = () => {
+  calculator();
+};
 // const num1 = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
 // const num2 = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
-const operators = ["+", "-", "*", "/"];
+const operators = ["+", "-", "*", "/", "√x", "^"];
 const display = document.getElementById("display");
-
-// for (let index = 0; index <= 9; index++) {
-//   const numberButtons = document.getElementById(`${index}`);
-//   if (index < 4) {
-//     const operatorButtons = document.getElementById(operators[index]);
-//     operatorButtons.addEventListener(
-//       "click",
-//       () => (display.textContent = `${operators[index]}`)
-//     );
-//   }
-//   numberButtons.addEventListener("click", () => {
-//     display.textContent = `${index}`;
-//     const numberButtons2 = document.getElementById(`${index} a`);
-//     numberButtons2.addEventListener("click", () => {
-//       display.textContent = `${index} a`;
-//     });
-//   });
-// }
-
-for (let index = 0; index <= 9; index++) {
-  const numberButtons = document.getElementById(`${index}`);
-  const equalButton = document.getElementById("equal__button");
-  numberButtons.addEventListener("click", () => {
-    display.textContent = `${index}`;
-    const num1 = parseInt(`${index}`);
-    for (let i = 0; i < 4; i++) {
-      const operatorButtons = document.getElementById(`${operators[i]}`);
-      operatorButtons.addEventListener("click", () => {
-        const operator = `${operators[i]}`;
-        display.textContent = `${num1} ${operator}`;
-        for (let j = 0; j <= 9; j++) {
-          const numberButtons2 = document.getElementById(`${j}`);
-          numberButtons2.addEventListener("click", () => {
-            const num2 = parseInt(`${j}`);
-            display.textContent = `${num1} ${operator} ${num2}`;
-            equalButton.addEventListener("click", () => {
-              display.textContent = `${operate(num1, num2, operator)}`;
+const acButton = document.getElementById("ac__button");
+const operation = document.getElementById("operation");
+const calculator = () => {
+  for (let index = 0; index <= 9; index++) {
+    acButton.addEventListener("click", () => {
+      display.textContent = 0;
+      calculator();
+    });
+    let numberButtons = document.getElementById(`${index}`);
+    const equalButton = document.getElementById("equal__button");
+    numberButtons.addEventListener("click", () => {
+      display.textContent = `${index}`;
+      const num1 = parseInt(`${index}`);
+      for (let i = 0; i < 6; i++) {
+        acButton.addEventListener("click", () => {
+          display.textContent = 0;
+          calculator();
+        });
+        const operatorButtons = document.getElementById(`${operators[i]}`);
+        operatorButtons.addEventListener("click", () => {
+          const operator = `${operators[i]}`;
+          display.textContent = `${num1} ${operator}`;
+          for (let j = 0; j <= 9; j++) {
+            acButton.addEventListener("click", () => {
+              display.textContent = 0;
+              calculator();
             });
-          });
-        }
-      });
-    }
-  });
-}
+            let numberButtons2 = document.getElementById(`${j}`);
+            numberButtons2.addEventListener("click", () => {
+              acButton.addEventListener("click", () => {
+                display.textContent = 0;
+                operation.style.cssText = "color: #8f8f8f; font-weight: 400";
+                calculator();
+              });
+              const num2 = parseInt(`${j}`);
+              display.textContent = `${num1} ${operator} ${num2}`;
+              equalButton.addEventListener("click", () => {
+                operation.textContent = `${num1} ${operator} ${num2} =`;
+                operation.style.cssText = "color: black; font-weight: 400";
+                display.textContent = `${operate(num1, num2, operator)}`;
+              });
+            });
+          }
+        });
+      }
+    });
+  }
+};
+calculator();
