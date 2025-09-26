b87917af6822225e086333945d85d2453565ca29
schismatiic <leonardojofre2003@gmail.com>
2025-09-26 14:38:42 -0300

    El segundo número se repite en base el length de array1
    El segundo número permite ser mayor a 9, pero se repite dependiendo del length de array1. Lo anterior se debe corregir

--- script.js
+++ script.js
@@ -26,11 +26,6 @@ const operate = (a, b, operator) => {
       break;
   }
 };
-const ac = () => {
-  calculator();
-};
-// const num1 = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
-// const num2 = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
 
 const operators = ["+", "-", "*", "/", "√x", "^"];
 const display = document.getElementById("display");
@@ -47,34 +42,40 @@ const calculator = () => {
   let array1 = [];
   let array2 = [];
   for (let index = 0; index <= 9; index++) {
+    // ========= First number =========
     let numberButtons = document.getElementById(`${index}`);
     const equalButton = document.getElementById("equal__button");
     numberButtons.addEventListener("click", () => {
-      array1.push(`${index}`);
-      let num1 = parseInt(array1.join(""));
+      array1.push(index);
+      let num1 = array1.join("");
       display.textContent = `${num1}`;
+      // ========= Operator =========
       for (let i = 0; i < 6; i++) {
         const operatorButtons = document.getElementById(`${operators[i]}`);
         operatorButtons.addEventListener("click", () => {
           const operator = `${operators[i]}`;
           if (`${operator}` === "√x") {
-            display.textContent = `${num1} ${operator}`;
+            operation.textContent = `√(${num1}) =`;
+            operation.style.cssText = "color: black; font-weight: 400";
+            display.textContent = `√(${num1})`;
             equalButton.addEventListener("click", () => {
-              operation.textContent = `${num1} ${operator} =`;
               operation.style.cssText = "color: black; font-weight: 400";
               display.textContent = `${squareRoot(num1)}`;
             });
           } else {
             display.textContent = `${num1} ${operator}`;
+            // ========= Second number =========
             for (let j = 0; j <= 9; j++) {
               let numberButtons2 = document.getElementById(`${j}`);
               numberButtons2.addEventListener("click", () => {
-                const num2 = parseInt(`${j}`);
+                array2.push(j);
+                let num2 = array2.join("");
                 display.textContent = `${num1} ${operator} ${num2}`;
                 equalButton.addEventListener("click", () => {
+                  const result = operate(num1, num2, operator);
                   operation.textContent = `${num1} ${operator} ${num2} =`;
                   operation.style.cssText = "color: black; font-weight: 400";
-                  display.textContent = `${operate(num1, num2, operator)}`;
+                  display.textContent = `${result}`;
                 });
               });
             }
